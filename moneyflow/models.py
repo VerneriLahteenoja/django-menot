92a7cd8775c81c8a8b1490ebfbd9aefa9ad60b40
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2023-11-06 13:06:51 +0200

    Add verbose names and str methods

--- moneyflow/models.py
+++ moneyflow/models.py
@@ -25,6 +25,12 @@ class Account(TimestampModel):
     name = models.CharField(max_length=200)
     bank_account = models.CharField(max_length=20, null=True, blank=True)
 
+    class Meta:
+        verbose_name = _("tili")
+        verbose_name_plural = _("tilit")
+
+    def __str__(self):
+        return f"{self.id:04d} {self.name}"
 
 class Document(TimestampModel, OwnedModel):
     class Type(models.TextChoices):
@@ -34,19 +40,34 @@ class Document(TimestampModel, OwnedModel):
         OTHER = ("OTHER", _("Muu"))
 
     document_type = models.CharField(max_length=20, choices=Type.choices)
+    document_name = models.CharField(max_length=100, blank=True)
     document_file = models.FileField(upload_to="docs/%Y-%m/")
 
+    class Meta:
+        verbose_name = _("dokumentti")
+        verbose_name_plural = _("dokumentit")
+
+    def __str__(self):
+        return self.document_name if self.document_name else f"Document {self.id}"
 
 
 class Category(TimestampModel, OwnedModel):
     category_name = models.CharField(max_length=100)
     parent = models.ForeignKey(
         "self", 
-        blank=True, 
-        related_name="children",
+        blank=True,
+        null=True,
+        related_name="subcategories",
         on_delete=models.CASCADE,
     )
 
+    def __str__(self):
+        prefix = f"{self.parent} / " if self.parent else ""
+        return f"{prefix}{self.category_name}"
+    
+    class Meta:
+        verbose_name = _("kategoria")
+        verbose_name_plural = _("kategoriat")
 
 class Transaction(TimestampModel):
     class Type(models.TextChoices):
@@ -61,6 +82,7 @@ class Transaction(TimestampModel):
     transaction_type = models.CharField(max_length=10, choices=Type.choices)
     transaction_state = models.CharField(max_length=10, choices=State.choices)
     transaction_date = models.DateField()
+    amount = models.DecimalField(max_digits=20, decimal_places=2)
     transaction_category = models.ForeignKey(
         Category, 
         null=True, 
@@ -72,5 +94,10 @@ class Transaction(TimestampModel):
         related_name="transactions",
         blank=True,
     )
-    
 
+    class Meta:
+        verbose_name = _("tilitapahtuma")
+        verbose_name_plural = _("tilitapahtumat")
+    
+    def __str__(self):
+        return f"{self.transaction_date} {self.account} {self.amount} ({self.transaction_state})"
