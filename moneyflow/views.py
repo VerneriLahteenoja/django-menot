5f73f248de8c848cea744c8eb360169cab1847b2
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2023-11-06 16:12:15 +0200

    create views for accounts and documents

--- moneyflow/views.py
+++ moneyflow/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from .models import Account
+from .models import Account, Document
 
 @login_required
 def frontpage(request):
-    
+    return render(request, "moneyflow/index.html")
+
+@login_required
+def accounts(request):
     accounts = Account.objects.filter(user=request.user)
     context = {
         "accounts": accounts
     }
+    return render(request, "moneyflow/account.html", context)
 
-    return render(request, "moneyflow/index.html", context)
+@login_required
+def documents(request):
+    docs = Document.objects.filter(owner=request.user)
+    context = {
+        "documents": docs
+    }
+    return render(request, "moneyflow/documents.html", context)
