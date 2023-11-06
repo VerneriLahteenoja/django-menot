d52bf0b7acad9a5054b9e2c5d3bde54d0c538b76
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2023-11-06 13:23:03 +0200

    Suomenna applikaation nimi

--- moneyflow/apps.py
+++ moneyflow/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
-
+from django.utils.translation import gettext_lazy as _
 
 class MoneyflowConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'moneyflow'
+    verbose_name = _("Rahavirrat")
