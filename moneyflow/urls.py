059812bffb4f0e038c9284fa761daabad4adfc7a
VerneriLahteenoja <verneri.lahteenoja@gmail.com>
2023-11-06 16:12:01 +0200

    Create urls for tabs

--- moneyflow/urls.py
+++ moneyflow/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.frontpage),
+    path('', views.frontpage, name="index"),
+    path('account/', views.accounts, name="account"),
+    path('document/', views.documents, name="document"),
 ]
